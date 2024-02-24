c62eef6572d021f29a1e2aedbb1c1c934bcb01bb
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-24 11:38:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,13 +34,13 @@ Add me as admin in your group and start filtering :)
 /id  -  Shows ID information
 /info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-© @learningbots79
+© @anmol0700
 """
 
 AboutTxT = """
-⭕️ Official Channel : @learningbots79</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ Official Channel : @Fim_Nest</b>
+⭕️ Developers : @anmol0700
+⭕️ Upgrader : @Film_Nest
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
